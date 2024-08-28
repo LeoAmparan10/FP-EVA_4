2838cbe67a04cb0cdcc498af213cba2a2bf469ee
LeoAmparan10 <L24550381@chihuahua2.tecnm.mx>
2024-08-28 17:45:13 -0600

    Add files via upload

---
+++ EVA1_4_TV.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_tv;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int size;
+        double precio;
+        String tecnologia;
+        
+        marca="SAMSUNG";
+        modelo="Neo QLED 8K";
+        size=85;
+        precio = 129.799;
+        tecnologia="QLED";
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(size);
+        System.out.println(precio);
+        System.out.println(tecnologia);
+    }
+
+    
+}
